4db4a0c78183b5b4d6bddf2271d70808734986bb
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-03-22 20:05:13 +0000

    add user and furniture routes

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 
+const userRouter = require('./routes/userRouter');
+const furnitureRouter = require('./routes/furnitureRouter');
+
 const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
+
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/furniture', furnitureRouter);
 
 module.exports = app;
