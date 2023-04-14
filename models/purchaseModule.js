0a782de69d32c1e9739d081f04521372c0834ef0
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-04-14 21:01:26 +0100

    create purchase module

---
+++ models/purchaseModule.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const purchaseSchema = new mongoose.Schema({
+  ownerId: {
+    type: mongoose.Types.ObjectId,
+    ref: 'User',
+  },
+  product: {
+    type: String,
+  },
+  date: {},
+  price: {
+    type: Number,
+  },
+  quantity: {
+    type: Number,
+  },
+  img: {
+    type: String,
+  },
+  shippingAddress: {},
+});
+
+const Purchase = mongoose.model('Purchase', purchaseSchema);
+
+module.exports = Purchase;
