e8ee2680bfae2111b37d727f4350e4b4a9eae72b
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-04-17 19:34:12 +0100

    return port

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ mongoose
   });
 
 const port = process.env.PORT || 3000;
-const server = app.listen( () => {
+const server = app.listen(port,'0.0.0.0', () => {
   console.log(`App running on port ${port}...`);
 });
 
