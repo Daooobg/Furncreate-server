35d1bca5bad13d187947650f79d509881f88c1a5
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-04-17 16:01:42 +0100

    remove port

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ mongoose
   });
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () => {
+const server = app.listen( () => {
   console.log(`App running on port ${port}...`);
 });
 
