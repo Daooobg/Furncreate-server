848987148bb4ca50bb0c2808c15387f6f50284dc
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-04-18 18:11:49 +0100

    return comment

--- controllers/furnitureController.js
+++ controllers/furnitureController.js
@@ -38,5 +38,6 @@ exports.createComment = catchAsync(async (req, res, next) => {
   const comment = await furnitureService.createComment(req.user, req.body);
   res.status(200).json({
     status: 'success',
+    comment
   });
 });
